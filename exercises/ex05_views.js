897dc479144ec384cb800fa0d8b7e4205674debf
Chai Phonbopit <phonbopit@gmail.com>
2016-03-02 22:40:51 +0700

    finish ex05

--- exercises/ex05_views.js
+++ exercises/ex05_views.js
@@ -2,11 +2,33 @@
 
 const hapi = require('hapi');
 const server = new hapi.Server();
+const path = require('path');
 
 server.connection({
   host: 'localhost',
   port: Number(process.argv[2] || 8080)
 });
+
+server.register(require('vision'), (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: path.join(__dirname, 'templates')
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: {
+    view: 'index.html'
+  }
+})
 
 server.start(() => {
   console.log(`Server running at : ${server.info.uri}`);
